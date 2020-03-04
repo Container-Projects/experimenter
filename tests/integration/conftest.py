dfb76e1bfb92effbc84376941359dbb646241367
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2020-03-04 15:10:39 -0500

    Simplify integration test workflow fixes #1914

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,9 +1,16 @@
+import os
+
 import pytest
 import requests
 from requests.packages.urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 
 from pages.home import Home
+
+
+@pytest.fixture
+def bugzilla_host():
+    return os.environ["BUGZILLA_HOST"]
 
 
 @pytest.fixture
@@ -15,8 +22,8 @@ def capabilities(capabilities):
 @pytest.fixture
 def firefox_options(firefox_options):
     """Set Firefox Options."""
-    firefox_options.headless = True
-    firefox_options.log.level = 'trace'
+    firefox_options.headless = not bool(os.environ.get("DISABLE_HEADLESS"))
+    firefox_options.log.level = "trace"
     return firefox_options
 
 
@@ -32,7 +39,7 @@ def _verify_url(request, base_url):
 
 
 @pytest.fixture
-def fill_overview(selenium, base_url):
+def fill_overview(selenium, base_url, bugzilla_host):
     selenium.get(base_url)
     home = Home(selenium, base_url).wait_for_page_to_load()
     experiment = home.create_experiment()
@@ -40,5 +47,5 @@ def fill_overview(selenium, base_url):
     experiment.short_description = "Testing in here"
     experiment.public_name = "Public Name"
     experiment.public_description = "Public Description"
-    experiment.bugzilla_url = "http://bugzilla.com/show_bug.cgi?id=1234"
+    experiment.bugzilla_url = f"{bugzilla_host}show_bug.cgi?id=1234"
     return experiment
